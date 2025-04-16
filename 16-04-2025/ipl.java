0b5916226c2417022802d42e6fe671602af2feb1
kratigya001 <kratigyabansal80@gmail.com>
2025-04-16 23:56:16 +0530

    add

---
+++ 16-04-2025/ipl.java
@@ -0,0 +1,27 @@
+package Swayam1-.16-04-2025;
+
+public class ipl {
+    import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc= new Scanner(System.in);
+		int x= sc.nextInt();
+		if(x>=7){
+		    System.out.println("thala");
+		}
+		else{
+		    System.out.println("boom");
+		}
+		
+
+	}
+}
+
+    
+}
