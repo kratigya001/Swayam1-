904671c0aabaf6c85c8b456855582270596029be
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-04-27 01:58:28 +0530

    Time: 75 ms (18.87%) | Memory: 52.9 MB (16.85%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,43 +1,47 @@
 class Solution {
-    public int[] prevSmaller(int[] arr){
-        int n = arr.length;
-        int[] prev = new int[n];
-        prev[0] = -1;
-        Stack<Integer> stack = new Stack<>();
-        stack.push(0);
-        for(int i=1;i<n;i++){
-            while(!stack.isEmpty() && arr[i]<=arr[stack.peek()]) stack.pop();
-            if(stack.isEmpty()) prev[i] = -1;
-            else prev[i] = stack.peek();
-            stack.push(i);
-        }
-        return prev;
-    }
-    public int[] nextSmaller(int[] arr){
-        int n = arr.length;
-        int[] next = new int[n];
-        next[n-1] = n;
-        Stack<Integer> stack = new Stack<>();
-        stack.push(n-1);
-        for(int i=n-2;i>=0;i--){
-            while(!stack.isEmpty() && arr[i]<arr[stack.peek()]) stack.pop();
-            if(stack.isEmpty()) next[i] = n;
-            else next[i] = stack.peek();
-            stack.push(i);
-        }
-        return next;
-    }
     public int sumSubarrayMins(int[] arr) {
-        int n = arr.length;
-        int[] prev = prevSmaller(arr);
-        int[] next = nextSmaller(arr);
-        int mod = (int)(1e9)+7;
-        int sum = 0;
+        int n=arr.length;
+        int mod=(int)1e9+7;
+        long sum=0;
+        int prev[]=prevsmaller(arr);
+        int next[]=nextsmaller(arr);
         for(int i=0;i<n;i++){
-            long contri = (long)(i-prev[i])*(long)(next[i]-i)*arr[i];
-            int contrifinal = (int)(contri%mod);
-            sum = (sum+contrifinal)%mod;
-        } 
-        return sum;
+            int p=i-prev[i];
+            int ne=next[i]-i;
+           // int mul= p*ne;
+            long res=(long)p*(long) ne * arr[i];
+            sum= (sum+ res)% mod;
+
+        }
+        return (int)sum;
+   
+    }
+    private int[] prevsmaller(int []arr){
+        int n=arr.length;
+        int []psmall= new int[n];
+
+        Stack<Integer>st= new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && arr[i]<=arr[st.peek()]){
+                st.pop();
+            }
+             psmall[i]= (st.isEmpty()?-1 :st.peek());
+            st.push(i);
+        }
+        return psmall;
+    }
+     private int [] nextsmaller(int []arr){
+        int n=arr.length;
+        int []nsmall= new int[n];
+
+        Stack<Integer>st= new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && arr[i]<arr[st.peek()]){
+                st.pop();
+            }
+             nsmall[i]= (st.isEmpty()? n :st.peek());
+            st.push(i);
+        }
+        return nsmall;
     }
 }
