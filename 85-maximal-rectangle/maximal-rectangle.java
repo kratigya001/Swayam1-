b89bea03fa7666207f80915b59f0fec952040bd3
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-05-03 11:37:50 +0530

    Time: 26 ms (21.55%) | Memory: 45.1 MB (99.25%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,63 +1,57 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
+        int m=matrix.length;
+        int n= matrix[0].length;
+        int sum[]= new int [n]; // columnwise 
+        int result=0;
 
-        int[] height = new int[matrix[0].length];
-        int result = 0;
+        //prefix sum
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] == '0'){
+                    sum[j]=0;
+                }
+                sum[j] += (matrix[i][j] -'0');
 
-        for(int i = 0; i< matrix.length; i++){
-            for(int j = 0; j< matrix[i].length; j++){
-                int val = matrix[i][j] - '0';
-                if(val == 0){
-                    height[j] = 0;                    
-                }
-                else{
-                    height[j] += val;
-                }
             }
-            int maxArea = largestArea(height);
-            result = Math.max(maxArea, result);
+        
+            int maxarea= largesthistogram(sum );
+            result= Math.max(result, maxarea);
         }
         return result;
-        
+
     }
+    private int largesthistogram(int [] nums){
+        //nsr
+        int n=nums.length;
 
-    static int largestArea(int[] nums){
-        int n = nums.length;
-
-        int[] nsl = new int[n];
-        Stack<Integer> stack = new Stack<>();
-        for(int i = 0; i< n; i++){
-
-            while(! stack.isEmpty() && nums[stack.peek()] >= nums[i]){
-                stack.pop();
+        int []nsr= new int [n];
+        Stack<Integer>st= new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty()&& nums[st.peek()] >= nums[i]){
+                st.pop();
             }
-            if(!stack.isEmpty())  nsl[i] = stack.peek();
-            else nsl[i] = -1;
-
-            stack.push(i);
+            nsr[i]=(st.isEmpty()? n: st.peek() );
+            st.push(i);
         }
-
-        int[] nsr = new int[n];
-        Stack<Integer> stack1 = new Stack<>();
-        for(int i = n-1; i>= 0; i--){
-
-            while(! stack1.isEmpty() && nums[stack1.peek()] >= nums[i]){
-                stack1.pop();
+        //nsl
+        int []nsl= new int [n];
+        Stack<Integer>st1 = new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!st1.isEmpty() && nums[st1.peek()] >= nums[i]){
+                st1.pop();
             }
-            if(!stack1.isEmpty())  nsr[i] = stack1.peek();
-            else nsr[i] = n;    // because from end side 
-
-            stack1.push(i);
+            nsl[i]= (st1.isEmpty() ? -1 :st1.peek());
+            st1.push(i);
         }
-
-        int max = 0;        // here we didnt store the width area anywhere
-        for(int i = 0; i< n; i++){
-            int width = nsr[i] - nsl[i] - 1;
-            int area = width * nums[i];
-            max = Math.max(max, area);
+        //width  --> j-i-1
+        int res=0;
+        for(int i=0;i<n;i++){
+            int width=nsr[i]- nsl[i] -1;
+            int area= width* nums[i];
+            res= Math.max(res, area);
         }
-
-        return max;
+        return res;
 
     }
 }
