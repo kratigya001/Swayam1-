435e80ae285652803ae8ad6bb7a3fe79ea244cc8
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-05-09 00:35:15 +0530

    Time: 6 ms (66.44%) | Memory: 45.3 MB (73.19%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,35 +1,31 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st= new Stack<>();
-      //  int sum=0;
+        Stack<Integer>st= new Stack<>();
         for(int a: asteroids){
-         
-            //coillion occur
-            while(!st.isEmpty() && a<0 && st.peek()>0 ){
-                 int sum=st.peek()+ a;
-                 if(sum >0){//positive
-                   a=0;//exploide
-
-                 }
-                 else if(sum <0 ){
-                    st.pop();
-                 }
-                 else{ //sum==0
+            //when collision
+            while(!st.isEmpty() && a <0 && st.peek()>0){
+                int sum= st.peek() + a ;
+                if(sum>0){
                     a=0;
+                }
+                else if(sum <0){
                     st.pop();
-                 }
+                }
+                else{// sum==0
+                    st.pop();
+                    a=0;
+                }
+
             }
             if(a!=0){
                 st.push(a);
             }
         }
-        int arr[]=new int [st.size()];
-      //  int new1 []= new int[st.size()];
-       
-        for(int i=arr.length-1;i>=0;i--){
-            arr[i]=st.pop();
+        //now convert st into arr
+        int res[]= new int[st.size()];
+        for(int i=st.size()-1;i>=0;i--){
+            res[i]=st.pop();
         }
-        return arr;
-        
+        return res;
     }
 }
