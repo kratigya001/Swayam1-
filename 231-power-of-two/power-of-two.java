bbc875d4be55840d34f56e7772f03b3aa11809ee
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-05-14 00:02:40 +0530

    Time: 1 ms (37.39%) | Memory: 40.6 MB (90.37%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,11 +1,8 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        for(int i=0;i<31;i++){
-            if(Math.pow(2,i) ==n){
-                return true;
-            }
-        }
-        return false;
+        return n>0 && (n & (n-1))==0;
         
     }
-}
+}//bitwise - 8=1000
+       //      7=0111 
+         //  and =0000
