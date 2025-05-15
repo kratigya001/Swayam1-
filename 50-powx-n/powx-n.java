dec7cac4e2cf610b4d5f67f973040312cc2562cc
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-05-15 12:51:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (10.65%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,7 +1,20 @@
 class Solution {
-    public double myPow(double x, int n) {
-       return Math.pow(x,n);
+    public double myPow(double x, int n) {// i am not using Math.pow()
+         return solve(x,(long)n);
         
-        
+    }
+    private double solve(double x, long n){
+        if(n==0) return 1; //base
+
+         if(n <0){ //-ve n
+           return solve((1/x) , -n);
+        }
+        else if( n %2 ==0){ //even n
+           return  solve( x*x , n/2);
+        }
+        else{ //odd
+           return  x* solve(x*x, (n-1)/2);
+        }
+
     }
 }
