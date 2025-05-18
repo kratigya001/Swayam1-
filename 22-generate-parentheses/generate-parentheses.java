ad3bb5f06207073725f19a9dc400fdce20db9696
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-05-18 12:12:15 +0530

    Time: 2 ms (47.08%) | Memory: 43.7 MB (15.32%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,28 +1,25 @@
 class Solution {
-    private List<String> result= new ArrayList<>();
+    private List<String>result= new ArrayList<>();
     public List<String> generateParenthesis(int n) {
-         solve(n,"",0,0);
+         solve("",n,0,0);
          return result;
-
         
     }
-    private void solve( int n,String curr,int open, int close){
-        if(curr.length()==2*n){
+    public void solve(String curr, int n,int open, int close){
+        //base case
+        if(curr.length()== 2*n){
             result.add(curr);
-            return;
+            return ;
         }
         if(open<n){
-            curr += '(';
-            solve(n,curr,open+1, close);
-            curr=curr.substring(0,curr.length()-1);//backtrack (last char remove)
+            curr+= '(';
+            solve(curr,n,open+1,close);
+            curr= curr.substring(0,curr.length()-1);
         }
-        if(close<open){
-            curr += ')';
-            solve(n,curr,open, close+1);
-            curr=curr.substring(0,curr.length()-1);
+          if(close< open){
+            curr+= ')';
+            solve(curr,n,open,close+1);
+           // curr= curr.substring(0,n);
         }
-
-
-
     }
 }
