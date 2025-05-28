44940a8e7a44fddd42d4b0eb35b1e10e60fe3ff2
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-05-29 00:18:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (99.32%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n=nums.length;
+        int i=0;
+        for( i=0;i<n;i++){
+            if(nums[i]>= target){
+                return i;
+            }
+        }
+        return i;
+
+        
+    }
+}
