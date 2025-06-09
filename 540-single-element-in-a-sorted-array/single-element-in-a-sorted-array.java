73bdcb59fece5e0a22b16d87a94d8e37092b03be
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-06-09 18:59:52 +0530

    Time: 0 ms (100.00%) | Memory: 50.4 MB (52.84%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,16 +1,34 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n=nums.length;
-        HashMap<Integer,Integer>hm= new HashMap<>();
-        for(int i=0;i<n;i++){
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+        int start=0;
+        int end=n-1;
+        if(n==1) return nums[0];
+       
+      
+        while(start <=end){
+            int mid=(start+end) /2;
+            if(mid==0 && nums[mid]!=nums[mid+1]) return nums[mid];
+            if(mid==n-1 && nums[mid]!=nums[n-2]) return nums[mid];
 
-        }
-        for(int key:hm.keySet()){
-            if(hm.get(key)==1){
-                return key;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
+
+            if(mid%2==0){
+                if(nums[mid]==nums[mid-1]){//left occur
+                    end=mid-1; 
+                }
+                else{  
+                    start=mid+1;  //right
+                }
             }
-
+            else{
+                if(nums[mid]== nums[mid-1]){
+                    start=mid+1; //left
+                }
+                else{
+                    end=mid-1;//right
+                }
+            }
         }
         return -1;
         
