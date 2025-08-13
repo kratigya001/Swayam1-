0c6aeae399bd5176bf24513cb5502b60799991ee
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-08-14 00:56:50 +0530

    Time: 2 ms (96.56%) | Memory: 42.1 MB (12.54%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isValid(String s) {
+        //keypoints
+       // 1.Stack mai character daalna hai 
+     //  2.String ko chararray bnao
         Stack<Character> st= new Stack<>();
         for(char ch:s.toCharArray()){
             if(st.isEmpty() || ch=='('||ch=='{'||ch=='['){
