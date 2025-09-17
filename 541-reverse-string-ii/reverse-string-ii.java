13c9275fc5b7170c0028cb09953924885f56a37e
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-09-17 20:41:01 +0530

    Time: 1 ms (82.66%) | Memory: 43.3 MB (95.24%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.java
+++ 541-reverse-string-ii/reverse-string-ii.java
@@ -1,24 +1,21 @@
 class Solution {
     public String reverseStr(String s, int k) {
-        //right is i+k-1 (yha tak swap hoga)
-        //left is i+2k (swap start)
-
-        char []a= s.toCharArray();
+        char[]a=s.toCharArray();
         int n=a.length;
-        for(int i=0;i<a.length; i+= 2* k){
-            int left=i;
-            int right=Math.min(i+k-1, n-1);
-
-            while(left<right){
+   
+        for(int i=0;i<n;i= i+2*k){
+             int left=i;
+            int right=Math.min(i+k-1,n-1);
+            while(left< right){
                 char temp=a[left];
                 a[left]=a[right];
                 a[right]=temp;
                 left++;
                 right--;
             }
+
         }
         return new String(a);
 
-        
     }
 }
