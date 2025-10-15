596aa13881ee78d59a1426b6adce66aab0ee3b95
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-10-15 23:53:26 +0530

    Time: 1 ms (63.05%) | Memory: 40.7 MB (64.71%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    // Helper function to get sum of squares of digits
+    private int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = getNext(n);
+
+        while (fast != 1 && slow != fast) {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        }
+
+        return fast == 1;
+    }
+}
