d38f001e56638fae3c05b772126fe0326083adf8
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-11-03 01:47:45 +0530

    Time: 87 ms (28.65%) | Memory: 43.7 MB (26.29%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,32 +1,38 @@
 class Solution {
-    private int mincount(int[] freq){
-        int ans=Integer.MAX_VALUE;
-        for(int i=0;i<26;i++){
-            if(freq[i]!=0){
-                ans=Math.min(ans,freq[i]);
+    public int beautySum(String s) {
+        int n=s.length();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            int freq[]=new int [26];
+            for(int j=i;j<n;j++){
+                freq[s.charAt(j)-'a']++ ;
+                int beauty= maxcount(freq)- mincount(freq);
+                sum+= beauty;
+
             }
         }
-        return ans;
+        return sum;        
     }
     private int maxcount(int[] freq){
         int ans=0;
         for(int i=0;i<26;i++){
             ans=Math.max(ans,freq[i]);
+        }
+        return ans;
+
+    }
+    private int mincount(int[] freq){
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<26;i++){
+          if(freq[i]!=0){
+             ans=Math.min(ans,freq[i]);
+          }
+          
 
         }
         return ans;
-    }
-    public int beautySum(String s) {
-        int n=s.length();
-        int sum=0;
-        for(int i=0;i<n;i++){
-            int freq[]=new int[26]; //bcz 26 charcters 
-            for(int j=i;j<n;j++){
-                freq[s.charAt(j)-'a']++;
-                int beauty= maxcount(freq) - mincount(freq);
-                sum+= beauty;      
-            }
-        }
-        return sum;  
+
+            
+        
     }
 }
