c28c8a3a4ecb91563f9422d66d957e8359a62ca6
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-11-02 23:27:17 +0530

    Time: 6 ms (71.12%) | Memory: 45.2 MB (15.53%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,28 +1,37 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> s = new Stack<>();
-        int i = 0;
-        while (i < tokens.length) {
-            String ch = tokens[i];
-            if (ch.equals("+")) {
-                s.push(s.pop() + s.pop());
-            } else if (ch.equals("-")) {
-                int b = s.pop(); // Second operand
-                int a = s.pop(); // First operand
-                s.push(a - b);
-            } else if (ch.equals("*")) {
-                s.push(s.pop() * s.pop());
-            } else if (ch.equals("/")) {
-                int b = s.pop(); // Second operand
-                int a = s.pop(); // First operand
-                s.push(a / b);
-            } else {
-                // s.push(Integer.parseInt(ch));
-                s.push(Integer.valueOf(ch));
+        Stack<Integer>st= new Stack<>();
+        int i=0;
+        while(i<tokens.length){
+            String ch=tokens[i];
+            if(ch.equals("+")){
+               int a=st.pop();
+               int b=st.pop();
+                st.push(a+b);
+            }
+            else if(ch.equals("-")){
+                int a=st.pop();//second
+                int b=st.pop();//first
+                st.push(b-a);
+
+            }
+              else if(ch.equals("*")){
+                int a=st.pop();
+                int b=st.pop();
+                st.push(a*b);
+
+            }
+              else if(ch.equals("/")){
+                int a=st.pop();//second
+                int b=st.pop();//first
+                st.push(b/a);
+            }
+            else{
+                st.push(Integer.valueOf(ch));
             }
             i++;
-        }
 
-        return s.pop();
+        }
+        return st.pop();
     }
 }
