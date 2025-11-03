408b1b6ad26db3d70e313a16f17d1555e665687c
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-11-04 00:44:29 +0530

    Time: 27 ms (36.14%) | Memory: 48 MB (6.31%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,41 +1,39 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int freq[]= new int [26];
-            int time=0;
-
-        //calculate freq
+        PriorityQueue<Integer>pq= new PriorityQueue<>(Collections.reverseOrder());
+        int freq[]= new int[26];
+        int time=0;
         for(char t:tasks){
-            freq[t-'A']++;
+            freq[t-'A']++; //calculate freq
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); //maxheap
         for(int f:freq){
-            if(f>0)  pq.add(f);
+            if(f>0){
+                pq.add(f); //pq m add
+            }
         }
-
         while(!pq.isEmpty()){
             ArrayList<Integer>temp= new ArrayList<>();
-            for(int i=1;i<=1+n;i++){//execute n+1 task
+            for(int i=1;i<= n+1 ;i++){
                if(!pq.isEmpty()){
-                    int top=pq.poll();
-                    top--;
-                    temp.add(top);
+                 int top=pq.poll();
+                 top--;
+                 temp.add(top);
                }
             }
-
-            for(int remaining:temp){
-                if(remaining >0) pq.add(remaining);
+            for(int remaining: temp){
+                if (remaining>0) pq.add(remaining);
             }
-        
-        
-             if(pq.isEmpty()){
-                time+=temp.size();
-             }
-             else{
-                time+= 1+n;
-             }
+            if(pq.isEmpty()){
+                time+= temp.size();
+
+            }
+            else{
+                time+= 1+n ;
+            }
+
         }
-             
         return time;
+
 
     }
 }
