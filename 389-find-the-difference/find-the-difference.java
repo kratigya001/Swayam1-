f0dc8a17557dd76d274ac8a7d201e152009c832c
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-11-07 02:08:09 +0530

    Time: 1 ms (99.46%) | Memory: 42.3 MB (15.89%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,19 +1,13 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        //ascii value 
-        int tsum=0;
-        int ssum=0;
-
-       for(char c:s.toCharArray()){
-          ssum+= c;
-       }
-
-        for (char a:t.toCharArray()){
-            tsum+= a;
-
+        //xor
+        int ans=0;
+        for (char c:s.toCharArray()){
+            ans=c^ans;
         }
-        return (char) (tsum-ssum);
-
-        
+        for(char c:t.toCharArray()){
+            ans=c^ans;
+        }
+        return (char)ans;
     }
 }
