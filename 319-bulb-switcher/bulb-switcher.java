90b6bf71f25aa64d152177de7389a066e6e6dc35
Kratigya Bansal <147174690+kratigya001@users.noreply.github.com>
2025-11-15 12:42:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (7.14%) - LeetSync

---
+++ 319-bulb-switcher/bulb-switcher.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        //bcz of perfect square -- bulb on at every square root
+        //odd- bulb on
+        //even- bulb off
+
+        return (int) Math.sqrt(n);
+
+        
+    }
+}
